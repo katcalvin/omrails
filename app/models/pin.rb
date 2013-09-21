48bf9b21dcc3c08daf23aa2d8f65a544f8662571
kat calvin <kambertc@gmail.com>
2013-09-20 22:17:11 -0400

    Add Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -3,4 +3,6 @@ class Pin < ActiveRecord::Base
 
   validates :description, presence: true
   #find more at guides.rubyonrails.org/active_record_validations_callbacks.html
+  belongs_to :user
+  validates :user_id, presence: true
 end
